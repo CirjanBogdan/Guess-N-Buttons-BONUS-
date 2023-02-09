8bce059d270bc3742dd24d1f290d80f1440b44fc
Lthz0R <124176929+Lthz0R@users.noreply.github.com>
2023-02-09 14:14:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function generateButtons() {
+    let element = document.getElementById("div");
+    let btns = document.getElementById("btns"); 
+    let input = document.getElementById("userInput").value;
+    const randomNumber = Math.floor(Math.random() * input + 1);
+    element.style.display = "none";
+    document.getElementById("chooseButton").innerHTML = "Choose the winning Button!";        
+    for (let i = 1; i <= input; ++i) {
+        const button = document.createElement('button');
+        button.className = "button";
+        let buttonNum = i;
+        button.addEventListener("click", function() {
+            if (randomNumber === buttonNum) {
+                document.getElementById("winOrLose").innerHTML = "YOU WIN!!!";
+            } else {
+                document.getElementById("winOrLose").innerHTML = "YOU LOST!";
+            }
+            btns.style.display = "none";
+            const restartButton= document.createElement('button');
+            restartButton.innerText= "Restart the game";
+            restartButton.className = "button";
+            document.body.appendChild(restartButton);
+            restartButton.addEventListener("click", function() {
+                location.reload();
+            });
+        });
+        button.innerHTML = "Button " + buttonNum;
+        btns.appendChild(button);
+    }
+}
